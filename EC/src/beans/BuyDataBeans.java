91f38af4b252aedfca3bc02b87e4a687c21da847
IshiiIsaku <45581362+IshiiIsaku@users.noreply.github.com>
2019-01-25 17:56:24 +0900

    Update BuyDataBeans.java

--- EC/src/beans/BuyDataBeans.java
+++ EC/src/beans/BuyDataBeans.java
@@ -21,6 +21,18 @@ public class BuyDataBeans  implements Serializable {
 	private int deliveryMethodPrice;
 
 
+
+
+	public BuyDataBeans(int i, int j, Date date, int k, int l, int m, String string) {
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+	public BuyDataBeans() {
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+
+	public BuyDataBeans(BuyDataBeans data) {
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
 	public int getId() {
 		return id;
 	}
@@ -72,4 +84,65 @@ public class BuyDataBeans  implements Serializable {
 	}
 
 
+	public void add() {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+	private int buyId;
+	private int itemId;
+
+
+
+	public int getBuyId() {
+		return buyId;
+	}
+	public void setBuyId(int buyId) {
+		this.buyId = buyId;
+	}
+	public int getItemId() {
+		return itemId;
+	}
+	public void setItemId(int itemId) {
+		this.itemId = itemId;
+	}
+
+	private String name;
+	private String detail;
+	private int price;
+	private String fileName;
+
+
+
+	public String getName() {
+		return name;
+	}
+	public void setName(String itemName) {
+		this.name = itemName;
+	}
+	public String getDetail() {
+		return detail;
+	}
+	public void setDetail(String detail) {
+		this.detail = detail;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int itemPrice) {
+		this.price = itemPrice;
+	}
+	public String getFileName() {
+		return fileName;
+	}
+	public void setFileName(String filename) {
+		this.fileName = filename;
+	}
+
+
+
+
+
 }
+
+
